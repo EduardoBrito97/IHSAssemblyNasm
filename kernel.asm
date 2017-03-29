0ebdb6b93203abcbb2497c5e8f4d934184b52488
Eduardo Barreto Brito <ebb2@cin.ufpe.br>
2017-03-28 23:58:14 -0300

    Add files via upload

--- kernel.asm
+++ kernel.asm
@@ -17,7 +17,7 @@ constantes:
 	tam_contato equ 87
 	tam_horizontal_limite equ 1280 ;Obs: uma linha é 640, botando 1280 ele fica uma linha mais grossa
 
-	linhas_uma_pag equ 60
+	linhas_uma_pag equ 30
 
 dados:
 	;Mensagens
@@ -184,39 +184,8 @@ checkPage:
 	mov dx, [linha_Ultima_msg]
 	xor ax, ax
 	add ax, linhas_uma_pag
-	div ax
-	xor cx, cx
-	cmp cx, dx
-	je incPage
-ret
-
-incPage:
-	;Indo para a próxima página
-	mov ax, [pag_Ultima_msg]
-	inc ax
-	mov [pag_Ultima_msg], ax
-
-	;Iniciando na linha 0 da próxima página
-	xor dx,dx
-	mov [linha_Ultima_msg], dx
-
-	;Limpando a tela
-	xor ax, ax
-	mov ds, ax
-	mov ah, 0
-	mov al, 12h
-	int 10h
-
-	mov ah, 0xb
-	mov bh, 0
-	mov bl, 0h
-	int 10h
-
-	mov ah, 2
-	mov bh, 0
-	mov dh, [linha_Ultima_msg]
-	mov dl, 0
-	int 10h
+	cmp ax, dx
+	jle clearCom
 ret
 
 setCursor:
@@ -559,14 +528,11 @@ editCom:
 jmp comand
 
 delCom:
-	mov di, mensagem_erro
+
 	call busca
-	cmp di,si
-	je comand
 
 	mov di, [ptr_contato_atual]
 	mov si, [ptr_ultimo_contato]
-	sub si, tam_contato
 	mov bx, tam_contato
 	mov dx, 0
 	substituicao:
